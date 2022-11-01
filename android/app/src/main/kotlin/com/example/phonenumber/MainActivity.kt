c3ac5d974e7631ddf8533bb08b3983c5da951dca
apiltd <masrafianam@gmail.com>
2022-11-01 12:32:16 +0600

    First Commit

--- android/app/src/main/kotlin/com/example/phonenumber/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/phonenumber/MainActivity.kt
@@ -15,11 +15,23 @@ class MainActivity: FlutterActivity() {
 
         channel= MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL);
         channel.setMethodCallHandler { call, result ->
-            var argument = call.arguments as Map<String, String>
-            var message = argument["msg"];
-            if (call.method=="method"){
-                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            var argument = call.arguments as Map<String, String>  //for show toast
+//            var message = argument["msg"];
+//            if (call.method == "method") {
+//                Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+//            }
+            if (call.method=="method"){  //this is for show text
+                val messageText= getMesaager();
+                if(messageText.isNotEmpty()) {
+                    result.success(messageText);
+                }else{
+                    result.error("MASRAFI", "This error message from kotlin code", null);
+                }
             }
-        }
+      }
+            }
+
+    private  fun getMesaager(): String {
+        return "This message from Kotlin Code"
     }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -17,49 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key}) : super(key: key);
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  static const channel = MethodChannel('com.example.phonenumber');
-  Future showToast() async {
-    final int show = await channel.invokeMethod('method',
-        <String, String>{'msg': "This is message from Flutter to Native"});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Call a Toast message from native code",
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                showToast();
-              },
-              child: const Text("Show Toast"),
-            )
-          ],
-        ),
-      ),
     );
   }
 }
